31e1f644c6f8e39bba1911a2ae09b4b904450cb8
dansilx <danilosilveira@msn.com>
2024-03-13 23:43:51 -0300

    salvar

--- ex005.php
+++ ex005.php
@@ -1,4 +1,18 @@
 <?php
+function imprimirRecibo($nome, $livro, $tipo, $dataEmprestimo) {
+    $dataDevolucao = new DateTime($dataEmprestimo);
+    if ($tipo == "professor"){
+        $dataDevolucao->modify('+10 day');
+    } else if ($tipo == "aluno") {
+        $dataDevolucao->modify('+3 day');
+    }
+
+    echo "Nome: " . $nome . "\n";
+    echo "Livro: " . $livro . "\n";
+    echo "Tipo: " .$tipo . "\n";
+    echo "Data do Empréstimo: " . $dataEmprestimo . "\n";
+    echo "Data para Devolução ". $dataDevolucao->format('d/m/Y') . "\n"; 
+}
 
 echo "Digite seu nome: ";
 $nome = readline();
@@ -9,7 +23,5 @@ $tipo = readline();
 echo "Digite a data do empréstimo (dd/mm/aaaa): ";
 $dataEmprestimo = readline();
 
-
-
-
+imprimirRecibo($nome, $livro, $tipo, $dataEmprestimo)
 ?>
